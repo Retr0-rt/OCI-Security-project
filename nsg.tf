66fcf33263f64284a6d3e4222d12f9a84bfecc76
Retr0 <abdelhamida880@gmail.com>
2026-07-18 22:51:02 +0100

    NSGs and Load balancer

---
+++ nsg.tf
@@ -0,0 +1,150 @@
+# the load balancer nsg configuration
+#####################################
+resource "oci_core_network_security_group" "lb_nsg" {
+    compartment_id = var.compartemt_id
+    vcn_id = oci_core_vcn.secure_vcn.id
+    display_name = "NSG_LB_St26"
+}
+
+resource "oci_core_network_security_group_security_rule" "lb_ingress_https" {
+    network_security_group_id = oci_core_network_security_group.lb_nsg.id
+    direction = "INGRESS"
+    protocol = "6"
+    source_type = "CIDR_BLOCK"
+    source = "0.0.0.0/0"
+    stateless = false
+    description = "accept traffic from the internet on port 443"
+
+    tcp_options {
+      source_port_range {
+        min = var.web_port
+        max = var.web_port
+      }
+    }
+}
+
+resource "oci_core_network_security_group_security_rule" "lb_egress_app_server" {
+    network_security_group_id = oci_core_network_security_group.lb_nsg.id
+    direction = "EGRESS"
+    protocol = "6"
+    destination_type = "NETWORK_SECURITY_GROUP"
+    destination = oci_core_network_security_group.app_nsg.id
+    stateless = false
+    description = "Allow outbound traffic only towards the app in the private subnet"
+
+    tcp_options {
+      destination_port_range {
+        min = var.web_port
+        max = var.web_port
+      }
+    }
+}
+
+# the bastion nsg configuration
+###############################
+resource "oci_core_network_security_group" "bastion_nsg" {
+  compartment_id = var.compartemt_id
+  vcn_id = oci_core_vcn.secure_vcn.id
+  display_name = "NSG_Bastion_St26"
+}
+
+resource "oci_core_network_security_group_security_rule" "bastion_egress_ssh" {
+  network_security_group_id = oci_core_network_security_group.bastion_nsg.id
+  direction = "EGRESS"
+  protocol = "6"
+  destination_type = "NETWORK_SECURITY_GROUP"
+  destination = oci_core_network_security_group.app_nsg.id
+  stateless = false
+  description = "Enforce sercurity by allowing administrative access to private resources only through bastion server"
+
+  tcp_options {
+    destination_port_range {
+      min = 22
+      max = 22
+    }
+  }
+}
+
+# the application server VM NSG
+###############################
+
+resource "oci_core_network_security_group" "app_nsg" {
+    compartment_id = var.compartemt_id
+    vcn_id = oci_core_vcn.secure_vcn.id
+    display_name = "NSG_App_St26"
+}
+
+resource "oci_core_network_security_group_security_rule" "app_ingress_from_bastion" {
+    network_security_group_id = oci_core_network_security_group.app_nsg.id
+    direction = "INGRESS"
+    protocol = "6"
+    source_type = "NETWORK_SECURITY_GROUP"
+    source = oci_core_network_security_group.bastion_nsg.id
+    stateless = false
+    description = "allow ssh traffic from the bastion server on port 22"
+
+    tcp_options {
+      source_port_range {
+        min = 22
+        max = 22
+      }
+    }
+}
+
+resource "oci_core_network_security_group_security_rule" "app_ingress_from_lb" {
+    network_security_group_id = oci_core_network_security_group.app_nsg.id
+    direction = "INGRESS"
+    protocol = "6"
+    source_type = "NETWORK_SECURITY_GROUP"
+    source = oci_core_network_security_group.lb_nsg.id
+    stateless = false
+    description = "allow https access from the load balancer"
+
+    tcp_options {
+      source_port_range {
+        min = var.web_port
+        max = var.web_port
+      }
+    }
+}
+
+resource "oci_core_network_security_group_security_rule" "app_egress_to_db" {
+    network_security_group_id = oci_core_network_security_group.app_nsg.id
+    direction = "EGRESS"
+    protocol = "6"
+    source_type = "NETWORK_SECURITY_GROUP"
+    source = oci_core_network_security_group.db_nsg.id
+    stateless = false
+    description = "allow outbound traffic toward the database from the app server"
+
+    tcp_options {
+      source_port_range {
+        min = var.db_port
+        max = var.db_port
+      }
+    }
+}
+
+# The autonomous database nsg configuration
+resource "oci_core_network_security_group" "db_nsg" {
+    compartment_id = var.compartemt_id
+    vcn_id = oci_core_vcn.secure_vcn.id
+    display_name = "NSG_DB_St26"
+}
+
+resource "oci_core_network_security_group_security_rule" "ingress_app_to_db" {
+    network_security_group_id = oci_core_network_security_group.db_nsg.id
+    direction = "INGRESS"
+    protocol = "6"
+    source_type = "NETWORK_SECURITY_GROUP"
+    source = oci_core_network_security_group.app_nsg.id
+    stateless = false
+    description = "isolate the database by only allowing only traffic from the app server on port 1521"
+
+    tcp_options {
+      source_port_range {
+        min = var.db_port
+        max = var.db_port
+      }
+    }
+}
--- varibales.tf
+++ varibales.tf
@@ -20,3 +20,13 @@ variable "private_cidr" {
   type    = string
   default = "10.0.2.0/16"
 }
+
+variable "web_port" {
+  type = number
+  default = 443
+}
+
+variable "db_port" {
+  type = number
+  default = 1521
+}
